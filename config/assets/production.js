e5ed170d8e558a2b362c1af17e4dfc29f73348a4
N8 <n8@Nathans-MacBook-Pro.local>
2017-02-11 22:28:17 -0800

    make it better

--- config/assets/production.js
+++ config/assets/production.js
@@ -14,18 +14,31 @@ module.exports = {
       ],
       js: [
         // bower:js
-        'public/lib/angular/angular.min.js',
-        'public/lib/angular-animate/angular-animate.min.js',
-        'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-        'public/lib/angular-messages/angular-messages.min.js',
+        'public/lib/angular/angular.js',
+        'public/lib/angular-animate/angular-animate.js',
+        'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+        'public/lib/ng-file-upload/ng-file-upload.js',
+        'public/lib/angular-messages/angular-messages.js',
         'public/lib/angular-mocks/angular-mocks.js',
-        'public/lib/angular-resource/angular-resource.min.js',
-        'public/lib/angular-ui-notification/dist/angular-ui-notification.min.js',
-        'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-        'public/lib/ng-file-upload/ng-file-upload.min.js',
+        'public/lib/angular-resource/angular-resource.js',
+        'public/lib/angular-ui-notification/dist/angular-ui-notification.js',
+        'public/lib/angular-ui-router/release/angular-ui-router.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/ui-router-extras/release/ct-ui-router-extras.js',
         'public/lib/moment-develop/moment.js',
         'public/lib/angular-bootstrap-calendar-master/dist/js/angular-bootstrap-calendar-tpls.js'
+        // 'public/lib/angular/angular.min.js',
+        // 'public/lib/angular-animate/angular-animate.min.js',
+        // 'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+        // 'public/lib/angular-messages/angular-messages.min.js',
+        // 'public/lib/angular-mocks/angular-mocks.js',
+        // 'public/lib/angular-resource/angular-resource.min.js',
+        // 'public/lib/angular-ui-notification/dist/angular-ui-notification.min.js',
+        // 'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+        // 'public/lib/ng-file-upload/ng-file-upload.min.js',
+        // 'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        // 'public/lib/moment-develop/moment.js',
+        // 'public/lib/angular-bootstrap-calendar-master/dist/js/angular-bootstrap-calendar-tpls.js'
 
         // endbower
       ]
